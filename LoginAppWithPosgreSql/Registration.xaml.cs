f75c810fffc3d9b6e8332c6cd37e8506e8af7231
Tohirjon Akhrarov <tohirjanakhrarov@gmail.com>
2021-03-06 21:41:41 +0500

    Добавьте файлы проекта.

---
+++ LoginAppWithPosgreSql/Registration.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Npgsql;
+
+namespace LoginAppWithPosgreSql
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+            loginInpt.TextChanged += LoginInpt_TextChanged;
+        }
+
+        private void LoginInpt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int textboxLength = loginInpt.Text.Length;
+            if (textboxLength != 0 || textboxLength > 3 )
+            {
+                string error = "";
+                errorsLabel.Content = error;
+            }
+
+            else if (textboxLength < 3)
+            {
+                string error = "Точно не правилный логин";
+                errorsLabel.Content = error;
+            }
+        }
+
+        private void regBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int userId =Convert.ToInt16(idInpt.Text);
+            string userLogin = loginInpt.Text;
+            string userPass = passInpt.Text;
+            int userRole = 1;
+
+            string connString = "Host=localhost;Port=7777;Database=postgres;Username=postgres;Password=13123866rT;Persist Security Info=True";
+            NpgsqlConnection connection = new NpgsqlConnection(connString);
+            connection.Open();
+            using (var insertCommand = new NpgsqlCommand("INSERT INTO public.users_list(user_id, user_login, user_pass, user_role_id) VALUES(@userId, @userLogin, @userPass, @userRole); ", connection))
+            {
+                insertCommand.Parameters.AddWithValue("userId", userId);
+                insertCommand.Parameters.AddWithValue("userLogin", userLogin);
+                insertCommand.Parameters.AddWithValue("userPass", userPass);
+                insertCommand.Parameters.AddWithValue("userRole", userRole);
+                insertCommand.ExecuteNonQuery();
+              
+            }
+
+
+        }
+    }
+}
---
+++ LoginAppWithPosgreSql/Window1.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace LoginAppWithPosgreSql
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+       
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void get_button_Click(object sender, RoutedEventArgs e)
+        {
+            string urlfortest = "http://api.openweathermap.org/data/2.5/weather?q=Tashkent&appid=07bdeab56b19a65145aa3def82f1253c";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlfortest);
+            request.Method = "Post";
+            request.ContentType = "application/json";
+            request.Headers.Add("ContentType", "application/json");
+            request.Headers.Add("Authorization", "Basic SUIjdW5pY29uMTAxODpBYTEyMzQ1Njo2ODI2");
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string newresponse;
+            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+            {   
+                newresponse = streamReader.ReadToEnd();
+
+            }
+
+            newtextbox.Text = newresponse;
+
+
+            request.Method = "Post";
+            request.ContentType  = "application/json";
+            request.Headers.Add("ContentType", "application/json");
+            request.Headers.Add("Authorization", "Basic SUIjdW5pY29uMTAxODpBYTEyMzQ1Njo2ODI2");
+
+            /*request.Headers.Add("Content-Type", "application/json");*/
+
+           
+
+            
+        }
+    }
+}
